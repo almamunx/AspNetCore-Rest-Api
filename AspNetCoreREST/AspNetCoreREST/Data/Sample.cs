dbc73cf11cdc3a1b49afe38f0d38023b47627c8e
Shiham <Shiham626@gmail.com>
2020-08-02 16:49:19 +0600

    Connected to database and data model created 🖴 🔗 resolved #1

---
+++ AspNetCoreREST/AspNetCoreREST/Data/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreREST.Data
+{
+    public partial class Sample
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
