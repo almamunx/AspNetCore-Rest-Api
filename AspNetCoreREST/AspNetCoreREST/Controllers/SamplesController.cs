8de32016c270923c4202c6d7642b53a1235849fd
Shiham <Shiham626@gmail.com>
2020-08-09 21:26:22 +0600

    Controller "Sample" added

---
+++ AspNetCoreREST/AspNetCoreREST/Controllers/SamplesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreREST.Data;
+
+namespace AspNetCoreREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamplesController : ControllerBase
+    {
+        private readonly WebAPIContext _context;
+
+        public SamplesController(WebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Samples
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sample>>> GetSample()
+        {
+            return await _context.Sample.ToListAsync();
+        }
+
+        // GET: api/Samples/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sample>> GetSample(int id)
+        {
+            var sample = await _context.Sample.FindAsync(id);
+
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            return sample;
+        }
+
+        // PUT: api/Samples/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSample(int id, Sample sample)
+        {
+            if (id != sample.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sample).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SampleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Samples
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Sample>> PostSample(Sample sample)
+        {
+            _context.Sample.Add(sample);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSample", new { id = sample.Id }, sample);
+        }
+
+        // DELETE: api/Samples/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Sample>> DeleteSample(int id)
+        {
+            var sample = await _context.Sample.FindAsync(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sample.Remove(sample);
+            await _context.SaveChangesAsync();
+
+            return sample;
+        }
+
+        private bool SampleExists(int id)
+        {
+            return _context.Sample.Any(e => e.Id == id);
+        }
+    }
+}
